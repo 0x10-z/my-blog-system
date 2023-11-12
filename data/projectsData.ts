30d05d469d487222f00315f43735bdb2a1e52fa4
Iker Ocio <iker@ikerocio.com>
2023-11-12 03:03:58 +0100

    More info added

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,15 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Retrogasteiz',
+    description: `Retrogasteiz es un proyecto personal inspirado en iniciativas similares, que presenta una colección de más de 100,000 imágenes del Archivo de Álava, digitalizadas por la Diputación Foral de Álava y publicadas bajo una licencia abierta. Este proyecto investiga nuevas formas de mejorar la visualización de archivos fotográficos públicos, integrando tecnologías de visión artificial y ofreciendo una experiencia de usuario moderna y optimizada. Retrogasteiz no solo proporciona acceso a un valioso patrimonio fotográfico sino que también demuestra el potencial de las bases de datos públicas para el desarrollo comunitario y la colaboración.`,
+    imgSrc: '/static/images/retrogasteiz_landscape.webp',
+    href: 'https://retrogasteiz.com',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Ez dago Gasteiz B',
+    description: `Ezdagogasteizb es una plataforma dedicada a la concienciación y educación ambiental en Vitoria-Gasteiz, que promueve prácticas sostenibles y ecológicas aplicables en la vida cotidiana. Este proyecto ofrece una serie de consejos prácticos y accesibles para adoptar hábitos más verdes, desde la gestión de residuos hasta la optimización del uso de recursos. Además, fomenta el apoyo al comercio local, destacando cómo las elecciones de consumo pueden influir positivamente en la economía local y el medio ambiente. Ezdagogasteizb busca empoderar a los ciudadanos para que tomen decisiones informadas y responsables que contribuyan a un futuro sostenible..`,
+    imgSrc: '/static/images/ezdago_landscape.webp',
+    href: 'https://ezdagogasteizb.ikerocio.com',
   },
 ]
 
